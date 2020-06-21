64ac07268e1e92ae572aeda7b9b3101662731d3e
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-21 23:39:30 +0530

    ADDED Welcome Messege

---
+++ src/main/java/com/bridgelabz/findmaximum/main/FindMaximum.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.findmaximum.main;
+
+public class FindMaximum {
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME");
+    }
+}
---
+++ src/test/java/com/bridgelabz/findmaximum/main/FindMaximumTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.findmaximum.main;
+
+import org.junit.Test;
+
+public class FindMaximumTest {
+    @Test
+    public void test() {
+        FindMaximum a = new FindMaximum();
+    }
+}
