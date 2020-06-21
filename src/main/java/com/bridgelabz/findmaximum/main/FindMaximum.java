46456fe2ce94137e85f9ae31a976402793e8c70c
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-22 02:02:54 +0530

    ADDED find max using Integers

--- src/main/java/com/bridgelabz/findmaximum/main/FindMaximum.java
+++ src/main/java/com/bridgelabz/findmaximum/main/FindMaximum.java
@@ -1,8 +1,18 @@
 package com.bridgelabz.findmaximum.main;
-
-public class FindMaximum {
-
-    public static void main(String[] args) {
-        System.out.println("WELCOME");
+import java.lang.*;
+public class FindMaximum  {
+    /**
+     * Check Max integer Number from Array
+     * @param array
+     * @param <Integer>
+     * @return
+     */
+    public <Integer extends Comparable> Integer giveMax(Integer[] array) {
+        Integer max =array[0] ;
+        for (Integer arrayElement : array) {
+            if (arrayElement.compareTo(max) > 0)
+                max = arrayElement;
+        }
+        return max;
     }
 }
--- src/test/java/com/bridgelabz/findmaximum/main/FindMaximumTest.java
+++ src/test/java/com/bridgelabz/findmaximum/main/FindMaximumTest.java
@@ -1,10 +1,22 @@
 package com.bridgelabz.findmaximum.main;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class FindMaximumTest {
+    FindMaximum obj;
+
+    @Before
+    public void init() {
+        obj = new FindMaximum();
+    }
+
     @Test
-    public void test() {
-        FindMaximum a = new FindMaximum();
+    public void passArrayOfInt_KeepFindingMaxatLast_ShouldReturnMaxNumber() {
+        Integer[] array = {1, 5, 3};
+        new FindMaximum();
+        Integer value = (Integer) obj.giveMax(array);
+        Assert.assertEquals(array[1],value);
     }
 }
